c72eb19b343f43ca058e61ca9feec44b36bd6edb
Tipz Team <47551974+TipzTeam@users.noreply.github.com>
2020-01-12 11:39:44 +0800

    added better rems

--- delworking.bat
+++ delworking.bat
@@ -1,4 +1,8 @@
 @echo off
-rem Remove working.bat
+rem ==============================
+rem Ez Adb Tools
+rem Script 1 - Remove working.bat
+rem and call start.bat
+rem ==============================
 del "working.bat"
 call start.bat
