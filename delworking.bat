9caaefefe5eb4e1f1006a52a36ebb35a01b2fd4b
Tipz Team <47551974+TipzTeam@users.noreply.github.com>
2020-01-17 21:49:37 +0800

    Update delworking.bat

--- delworking.bat
+++ delworking.bat
@@ -4,14 +4,14 @@ rem Ez Adb Tools
 rem Script 1 - Remove working.bat
 rem and call start.bat
 rem ==============================
-if %delworkingbat%==0 (
+if %delworkingbat%==1 (
   echo Why are you here, if you launched this not using the start.bat,
   echo Please don't do that please!
   pause
   call start.bat
 ) else (
-  set delworkingbat=1
   echo Removing working.bat...
   del "working.bat"
+  set delworkingbat=0
   call start.bat
 )
