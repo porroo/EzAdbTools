8b3a1c1868812f70f9db177781d81ebbf63d9652
Tipz Team <47551974+TipzTeam@users.noreply.github.com>
2020-01-17 21:54:00 +0800

    Update delworking.bat

--- delworking.bat
+++ delworking.bat
@@ -6,8 +6,10 @@ rem and call start.bat
 rem ==============================
 if %delworkingbat%==1 (
   echo Why are you here, if you launched this not using the start.bat,
-  echo Please don't do that please!
-  pause
+  echo Please don't do that again as it might break something but luckily,
+  echo we made a system to prevent it from ever happening but please stil
+  echo don't launch this!
+  set /p pressanykey="Press any key to return to the menu..."
   call start.bat
 ) else (
   echo Removing working.bat...
