9116164e78e8a11b150b7749c5eb9042ee766655
Tipz Team <47551974+TipzTeam@users.noreply.github.com>
2020-02-03 12:03:42 +0800

    Add files via upload

---
+++ scripts/delworking.bat
@@ -0,0 +1,23 @@
+@echo off
+rem ==============================
+rem Ez Adb Tools
+rem Script 1 - Remove working.bat
+rem and call start.bat
+rem ==============================
+if %delworkingbat%==0 (
+  echo Warning
+  echo ====================
+  echo Why are you here, if you launched this not using the start.bat,
+  echo Please don't do that again as it might break something but luckily,
+  echo we made a system to prevent it from ever happening but please still
+  echo don't launch this!
+  set /p pressanykey="Press any key to return to the menu..."
+  call start.bat
+) else if %delworkingbat%==1 (
+  echo File remover
+  echo ====================
+  echo Removing working.bat...
+  del "working.bat"
+  set delworkingbat=0
+  call start.bat
+)
