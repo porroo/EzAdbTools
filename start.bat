aaa017c39470e2b541eb3de0025cf2776647071a
Tipz Team <47551974+TipzTeam@users.noreply.github.com>
2020-02-15 12:30:59 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -42,6 +42,7 @@ set sideload_zip=
 rem Set variables
 set productnamever=Ez Adb Tools Test Build 202002040941HKT
 set productnamever=%productnamever:"=% 
+goto menu
 
 rem adb.exe check
 echo Checking for adb.exe...
