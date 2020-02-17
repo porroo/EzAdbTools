a58778328b0ae78b04f047cb8f2ef1fd24b039a3
Tipz Team <47551974+TipzTeam@users.noreply.github.com>
2020-02-17 21:52:28 +0800

    Update eat.bat

--- bin/eat.bat
+++ bin/eat.bat
@@ -1,2 +1,8 @@
+@echo off
+:: ==============================
+:: Ez Adb Tools
+:: Script 2 - Start batch script
+:: after using the command line
+:: ==============================
 cd ..
 call start.bat
