efcafa5f057747160ff21856de5e3a1f27f74fc8
Tipz Team <47551974+TipzTeam@users.noreply.github.com>
2020-05-04 10:41:40 +0800

    [eat] Better return

--- bin/eat.bat
+++ bin/eat.bat
@@ -1,8 +1,11 @@
 @echo off
 :: ==============================
-:: Ez Adb Tools
+:: EzAdbTools
 :: Script 2 - Start batch script
 :: after using the command line
 :: ==============================
+echo Restarting EzAdbTools
+choice /d y /t 2 > nul
+adb kill-server
 cd ..
 call start.bat
